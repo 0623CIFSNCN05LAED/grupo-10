f6300a6fc5ce50833a9eb6766db35411f1ef8f2a
PabloGomezCardozo <pabligomez@gmail.com>
2023-09-14 14:12:47 -0300

    form productEdit

--- src/routes/product-router.js
+++ src/routes/product-router.js
@@ -1,8 +1,9 @@
-const {Router} = require ("express");
+const { Router } = require("express");
 const productController = require("../controllers/product-controller");
-const router = Router ();
+const router = Router();
 router.get("/productCart", productController.productCart);
 router.get("/productDetail", productController.productDetail);
-router.get("/productCreate",productController.productCreate );
+router.get("/productCreate", productController.productCreate);
+router.get("/productEdit", productController.productEdit);
 
-module.exports = router
+module.exports = router;
