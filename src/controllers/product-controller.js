dbf2fded9976875605de5b7f44698deffd549d87
Stiven-Yepes-Arenas <estivenyepesarenas@gmail.com>
2023-09-23 06:56:35 -0500

    configuracio metodo post formulario de creacion

--- src/controllers/product-controller.js
+++ src/controllers/product-controller.js
@@ -1,6 +1,7 @@
 const path = require("path");
 const productService = require("../services/productService")
 module.exports = {
+  
   products:(req, res) =>{
     const productsLenovo = productService.getProductsLenovo();
     const productsApple = productService.getProductsApple();
@@ -18,8 +19,16 @@ module.exports = {
   productDetail: (req, res) => {
     res.render("products/productDetail");
   },
+
+  // VISTA FORMULARIO DE CREACION PRODUCTO
   productCreate: (req, res) => {
     res.render("products/productCreate");
+  },
+  //METODO DE CREACION DE PRODUCTOS
+  createProduct:(req, res) => {
+    const product = req.body;
+    console.log(product);
+    res.redirect("products/products");
   },
   productEdit: (req, res) => {
     res.render("products/productEdit");
--- src/routes/product-router.js
+++ src/routes/product-router.js
@@ -1,9 +1,17 @@
 const { Router } = require("express");
 const productController = require("../controllers/product-controller");
 const router = Router();
+//PRODUCTOS EN GENERAL
 router.get("/products/", productController.products)
+
+//CARRITO DE PRODUCTO
 router.get("/cart/", productController.productCart);
+
+//CREACION
 router.get("/create/", productController.productCreate);
+router.post("/", productController.createProduct);
+
+//EDICION DE PRODUCTO
 router.get("/:id/", productController.productDetail);
 router.get("/edit/:id/", productController.productEdit);
 
