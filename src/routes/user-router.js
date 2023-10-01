5fef50a36d627a1f7b57189f23e212ec5356ff24
PabloGomezCardozo <pabligomez@gmail.com>
2023-10-01 13:14:27 -0300

    implements multer in form register

--- src/routes/user-router.js
+++ src/routes/user-router.js
@@ -1,14 +1,30 @@
+// ************ Require's ************
 const { Router } = require("express");
-const userController = require("../controllers/user-controller");
 const router = Router();
+const path = require("path");
 
+const multer = require("multer");
+
+const storage = multer.diskStorage({
+  destination: path.join(__dirname, "../../public/images/users"),
+  filename: function (req, file, cb) {
+    cb(
+      null,
+      file.fieldname + "-" + Date.now() + path.extname(file.originalname)
+    );
+  },
+});
+
+const upload = multer({ storage: storage });
+
+// ************ Controller Require ************
+const userController = require("../controllers/user-controller");
+
+//USUARIOS EN GENERAL
 router.get("/login/", userController.login);
 
 //CREACION DE USUARIO
 router.get("/register/", userController.register);
-router.post(
-  "/register/",
-  /*upload.single("imagen"),*/ userController.createUser
-);
+router.post("/register/", upload.single("avatar"), userController.createUser);
 
 module.exports = router;
