994abdbee192f1b5a1a7142c253a29b0222cca0c
PabloGomezCardozo <pabligomez@gmail.com>
2023-10-15 14:31:36 -0300

    Fixes validate-form-register, add user_name for session

--- src/middlewares/validate-form-register.js
+++ src/middlewares/validate-form-register.js
@@ -1,28 +1,32 @@
 const { validationResult } = require("express-validator");
-const users = require('../data/users/users')
+const users = require("../data/users/users");
 module.exports = {
-campo: (req, res, next) => {
-  const resultValidation = validationResult(req);
-  
-  if (resultValidation.errors.length > 0) {
-    return res.render("users/register", {
-      errors: resultValidation.mapped(),
-      oldData: req.body,
-    });
-  }
-  next()
-},
+  campo: (req, res, next) => {
+    const resultValidation = validationResult(req);
+
+    if (resultValidation.errors.length > 0) {
+      let user_name = null;
+      const data = req.session.userData;
+      if (data) {
+        user_name = data.user_name;
+      }
+      return res.render("users/register", {
+        errors: resultValidation.mapped(),
+        oldData: req.body,
+        user_name,
+      });
+    }
+    next();
+  },
 
   correo: (req, res, next) => {
-    const {email} = req.body;
-  if (users.findByField('email', email)) {
-        return res
-          .status(400)
-          .json({
-            error:
-              "El correo electrónico ya está registrado. Por favor, elija otro.", //to do have the alert appear in the field email
-          });
-      }
-  next();
-    },
-}
+    const { email } = req.body;
+    if (users.findByField("email", email)) {
+      return res.status(400).json({
+        error:
+          "El correo electrónico ya está registrado. Por favor, elija otro.", //to do have the alert appear in the field email
+      });
+    }
+    next();
+  },
+};
