cf280a33c1b85f0e901f9924332091140afaadd0
PabloGomezCardozo <pabligomez@gmail.com>
2023-10-28 11:19:59 -0300

    fixes product-services brand and multer image in product-routes

--- src/services/productService.js
+++ src/services/productService.js
@@ -4,24 +4,24 @@ const productService = {
   getProductsLenovo: () => {
     const products = db.products
       .getProducts()
-      .filter((product) => product.marca == "Lenovo");
+      .filter((product) => product.brand == "Lenovo");
     return products;
   },
   getProductsApple: () => {
     const products = db.products
       .getProducts()
-      .filter((product) => product.marca == "Apple");
+      .filter((product) => product.brand == "Apple");
     return products;
   },
   getProductsAsus: () => {
     const products = db.products
       .getProducts()
-      .filter((product) => product.marca == "ASUS");
+      .filter((product) => product.brand == "ASUS");
     return products;
   },
-    getProduct: (id) => {
-      const product = db.products.findById(id);
-      return product;
+  getProduct: (id) => {
+    const product = db.products.findById(id);
+    return product;
   },
   produtcCreating: (product) => {
     db.products.create(product);
