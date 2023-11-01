298e721dc7bac8ecbecd4340c3b28eb77b290b49
PabloGomezCardozo <pabligomez@gmail.com>
2023-11-01 13:13:10 -0300

    modifies middlewares guest_guard and user_log

--- src/middlewares/guest-guard.js
+++ src/middlewares/guest-guard.js
@@ -1,7 +1,13 @@
+const users = require("../data/users/users");
+
 module.exports = (req, res, next) => {
   if (!req.session.userData) {
     next();
   } else {
-    res.redirect("/");
+    const data = req.session.userData;
+    const email = data.user_name;
+    const user = users.findByEmail(email);
+    const user_id = user.id;
+    res.redirect("/users/" + user_id);
   }
 };
