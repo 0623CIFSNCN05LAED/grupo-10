d8f909ae237f3578c9fbaa8f7e3808384ce14bf5
PabloGomezCardozo <pabligomez@gmail.com>
2023-11-05 10:58:01 -0300

    modifies navbar login icon and implements dropdown menu in partials header

--- src/middlewares/user_log.js
+++ src/middlewares/user_log.js
@@ -7,6 +7,7 @@ module.exports = (req, res, next) => {
     const user = users.findByEmail(email);
     const user_first_name = user.first_name;
     res.locals.user_log = user_first_name;
+    res.locals.user = user;
   } else {
     res.locals.user_log = null;
   }
