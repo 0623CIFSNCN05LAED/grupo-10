ae0a2911c3ce13913150817fcd894d90b1221059
Stiven-Yepes-Arenas <estivenyepesarenas@gmail.com>
2023-11-06 00:26:36 -0500

    agregando las vistas para las categorias de productos

--- src/routes/product-router.js
+++ src/routes/product-router.js
@@ -23,6 +23,8 @@ router.get("/razer", productController.razer);
 
 //PRODUCTO POR CATEGORIA
 router.get('/pcs', productController.productsCategoryPcs);
+router.get('/celulares', productController.productsCategoryCelulares);
+router.get('/accesorios', productController.productsCategoryAccesosrios);
 //CARRITO DE PRODUCTO
 router.get("/cart/", userGuard, productController.productCart);
 
