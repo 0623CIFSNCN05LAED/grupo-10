7bbc5f06dd659a27dda96c8c4f56988296a5684c
PabloGomezCardozo <pabligomez@gmail.com>
2023-11-06 16:08:50 -0300

    modifies middlewares in profile route

--- src/middlewares/user_log.js
+++ src/middlewares/user_log.js
@@ -5,16 +5,16 @@ module.exports = (req, res, next) => {
 
   let emailInCookie = req.cookies.userEmail;
 
-  if (emailInCookie) {
-    let userFromCookie = users.findByEmail(emailInCookie);
-    res.locals.user_log = userFromCookie.first_name;
-    res.locals.user = userFromCookie;
-  } else if (data) {
+  if (data) {
     const email = data.user_name;
     const user = users.findByEmail(email);
     const user_first_name = user.first_name;
     res.locals.user_log = user_first_name;
     res.locals.user = user;
+  } else if (emailInCookie) {
+    let userFromCookie = users.findByEmail(emailInCookie);
+    res.locals.user_log = userFromCookie.first_name;
+    res.locals.user = userFromCookie;
   } else {
     res.locals.user_log = null;
     res.locals.user = null;
