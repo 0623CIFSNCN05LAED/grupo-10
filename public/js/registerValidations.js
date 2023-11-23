874adfa01f8c061a2c633e8370b1b953e5f90923
Stiven-Yepes-Arenas <estivenyepesarenas@gmail.com>
2023-11-22 22:58:55 -0500

    Se completan la mayoria de validaciones obligatorias quedando pendiente solo validacion de extenciones

--- public/js/registerValidations.js
+++ public/js/registerValidations.js
@@ -1,4 +1,4 @@
-// Poner en un array los objetos que quiero validar
+
 const validations = [
     {
         field: "first_name",
@@ -8,54 +8,61 @@ const validations = [
     {
         field: "last_name",
         check: (input) => input.value.length >= 2,
-        message: "El nombre debe tener al menos dos caracteres",
+        message: "El apellido debe tener al menos dos caracteres",
     },
     {
         field: "email",
-        check: (input) => validator.isEmail(input.value),
+        check: (input) => emailRegex.test(input.value),
         message: "Debe ser un correo electrónico válido",
     },
     {
         field: "password",
         check: (input) => input.value.length >= 8,
-        message: "El nombre debe tener al menos ocho caracteres",
+        message: "La contraseña debe tener al menos ocho caracteres",
     },
     {
         field: "password_repeat",
         check: (input) => input.value.length >= 8,
-        message: "El nombre debe tener al menos ocho caracteres",
-        //pendiente la validadcion de avatar 
+        message: "La contraseña repetida debe tener al menos ocho caracteres",
     },
+    // {
+    //     validacion de extencion
+    // },
 ];
-//Hacer foreach
+
+
 validations.forEach((validation) => {
     const inputId = validation.field;
     const input = document.getElementById(inputId);
     const inputErrorMsg = document.getElementById(inputId + "Error");
 
     function validate() {
-        
-        if(!input.value){
-            inputErrorMsg.innerText = "El campo no debe estar vacio"
-        }
+        inputValidation(validation, input, inputErrorMsg);
     }
 
     input.addEventListener("blur", validate);
     input.addEventListener("input", validate);
 });
-// const form = document.getElementById('form');
 
-// form.addEventListener('submit', (event) => {
-//     event.preventDefault();
-//     const first_name = document.getElementById("first_name");
+const form = document.getElementById('form');
+form.addEventListener('submit', (event) => {
+    event.preventDefault();
+    // Aquí puedes agregar lógica adicional si es necesario antes de enviar el formulario
+});
 
+function inputValidation(validation, input, inputErrorMsg) {
+    if (!input.value) {
+        inputErrorMsg.innerText = "El campo no debe estar vacío";
+        inputErrorMsg.classList.add("display");
+        return;
+    }
 
-//     if (first_name) {
-        
-//         const first_nameValue = first_name.value;
-//         if (first_nameValue.length >= 5) {
-//         } else {
-//             console.error('Error: La longitud del nombre debe ser al menos 5 caracteres');
-//         }
-//     }
-// });
+    if (!validation.check(input)) {
+        inputErrorMsg.innerText = validation.message;
+        inputErrorMsg.classList.add("display");
+        return;
+    }
+
+    inputErrorMsg.innerText = '';
+    inputErrorMsg.classList.remove('display');
+}
