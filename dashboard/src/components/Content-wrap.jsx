899d93dc7d7c37844d8ec9ccbd04e226bddc4724
PabloGomezCardozo <pabligomez@gmail.com>
2023-12-10 14:53:48 -0300

    refactor component Products with hooks

--- dashboard/src/components/Content-wrap.jsx
+++ dashboard/src/components/Content-wrap.jsx
@@ -1,14 +1,17 @@
 import { Route, Switch } from "react-router-dom";
 import Products from "./products/Products";
+import Statistics from "./Content/Statistics";
 
 export default function ContentWrap() {
   return (
     <main className="content-wrap" style={{ maxHeight: "calc(100vh - 6rem)" }}>
       <Switch>
-        <Route path="/" exact>
+        <Route path="/products">
           <Products />
         </Route>
-        <Route path="/statistics"></Route>
+        <Route path="/statistics">
+          <Statistics />
+        </Route>
         <Route path="*">
           <p>404 - página no encontrada</p>
         </Route>
