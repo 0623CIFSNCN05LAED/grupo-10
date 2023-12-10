19eab9c98ea16acf5d367b091d6e67acbe670b9f
PabloGomezCardozo <pabligomez@gmail.com>
2023-12-10 15:56:55 -0300

    Statitics component functional with hooks, without api users

--- dashboard/src/components/Content/Statistics.jsx
+++ dashboard/src/components/Content/Statistics.jsx
@@ -1,30 +1,52 @@
+import { useEffect, useState } from "react";
 import SmallCard from "./SmallCard";
 
-const myStats = [
-  {
-    id: 1,
-    title: "Cantidad de estrenos",
-    value: "15",
-    color: "cornflowerblue",
-    icon: "bi bi-film",
-  },
-  {
-    id: 2,
-    title: "Categorías",
-    value: "12",
-    color: "orange",
-    icon: "bi bi-tags-fill",
-  },
-  {
-    id: 3,
-    title: "Total ventas",
-    value: "$ 489.567",
-    color: "green",
-    icon: "bi bi-currency-dollar",
-  },
-];
-
 export default function Statistics() {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await fetch("http://localhost:4001/api/products");
+      const result = await response.json();
+      setProducts(result.meta);
+    };
+    fetchData();
+  }, []);
+
+  const countProducts = products.count;
+  const countCategories = () => {
+    if (products && products.countByCategory) {
+      const categories = Object.values(products.countByCategory);
+      return categories.length;
+    }
+    return 0;
+  };
+  console.log("categories: ", countCategories);
+
+  const myStats = [
+    {
+      id: 1,
+      title: "Cantidad de Productos",
+      value: countProducts,
+      color: "cornflowerblue",
+      icon: "bi bi-laptop",
+    },
+    {
+      id: 2,
+      title: "Categorías de Productos",
+      value: countCategories(),
+      color: "orange",
+      icon: "bi bi-tags-fill",
+    },
+    {
+      id: 3,
+      title: "Total de Usuarios",
+      value: "0 hardcodeado",
+      color: "green",
+      icon: "bi bi-people-fill",
+    },
+  ];
+
   return (
     <section className="content">
       <h2 className="mt-3">Estadísticas</h2>
