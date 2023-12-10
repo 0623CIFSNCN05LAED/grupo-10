bd579f75d304b2eeab51a6d44d49b3239a1aa60d
PabloGomezCardozo <pabligomez@gmail.com>
2023-12-10 10:11:49 -0300

    install react router dom and implements BrowserRouter in App.jsx

--- dashboard/src/App.jsx
+++ dashboard/src/App.jsx
@@ -1,16 +1,12 @@
-// import { useState } from "react";
-// import reactLogo from './assets/react.svg'
-// import viteLogo from '/vite.svg'
 import "./App.css";
 import Products from "./components/products/Products";
+import { BrowserRouter } from "react-router-dom";
 
 function App() {
-  // const [count, setCount] = useState(0)
-
   return (
-    <>
+    <BrowserRouter>
       <Products />
-    </>
+    </BrowserRouter>
   );
 }
 
