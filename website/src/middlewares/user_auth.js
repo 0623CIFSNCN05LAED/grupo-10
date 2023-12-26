7c1af6f2afecee422d7bb81944a28b59f45320d0
PabloGomezCardozo <pabligomez@gmail.com>
2023-12-26 12:14:46 -0300

    middleware user_auth simplifies code

--- website/src/middlewares/user_auth.js
+++ website/src/middlewares/user_auth.js
@@ -1,27 +1,11 @@
 const userService = require("../services/userService");
 
 module.exports = async (req, res, next) => {
-  const data = req.session.userData;
-  let emailInCookie = req.cookies.userEmail;
-  let email = "";
-  let user_id = "";
-  const route_user_id = req.params.id;
-  let isChecked = false;
+  const email = req.session.userData?.user_name || req.cookies.userEmail;
+  const user = await userService.userByEmail(email);
+  const routeUserId = req.params.id;
 
-  if (data) {
-    email = data.user_name;
-    const user = await userService.userByEmail(email);
-    user_id = user.id;
-    isChecked = user_id === route_user_id;
-  } else if (emailInCookie) {
-    req.session.userData = { user_name: emailInCookie };
-    email = emailInCookie;
-    const userInCookie = await userService.userByEmail(email);
-    user_id = userInCookie.id;
-    isChecked = user_id === route_user_id;
-  }
-
-  if (isChecked) {
+  if (user.id === routeUserId) {
     next();
   } else {
     res.redirect("/");
